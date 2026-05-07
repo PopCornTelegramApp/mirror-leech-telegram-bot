c7c79d8905a094a0137c5d572d6f83062ed9119d
PopCornTelegramApp <alizawali1234@gmail.com>
2026-05-07 11:03:23 +0100

    fix: start aria2/qbt/sabnzbd before bot on HF Space

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# Start aria2, qBittorrent, SABnzbd as background daemons
+bash aria-nox-nzb.sh
+
+# Wait for services to be ready
+sleep 5
+
+# Activate venv and start the bot
 source mltbenv/bin/activate
 python3 update.py
 python3 -m bot
